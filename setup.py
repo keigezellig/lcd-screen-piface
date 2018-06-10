d617b820abdfab69a29e2a1f27759ba43402844c
keigezellig <mh.joosten666@gmail.com>
2018-06-10 18:11:13 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from distutils.core import setup
 
 setup(
     name='lcd-screen-piface',
-    version='2.0.1',
-    packages=['blinker',
+    version='1.0.0',
+    packages=['pager', 'simple_pager'],
+    install_requires=['blinker',
     'git+https://github.com/piface/pifacecad/#egg=pifacecad'
     'git+https://github.com/piface/pifacecommon/#egg=pifacecommon'
-    'python-lirc==1.2.1'],
+    'python-lirc==1.2.1']
+
     url='https://github.com/keigezellig/lcd-screen-piface',
     license='THE APPRECIATION LICENSE',
     author='M. Joosten',
